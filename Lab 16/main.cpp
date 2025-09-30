843cbade24b98a9e253b315940fd0eec6d5b98dd
Nafi-GOAT <nafih2989@gmail.com>
2025-09-29 22:35:29 -0700

    Initial Commit

---
+++ Lab 16/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 16
+//
+//  Created by Nafi on 9/29/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
